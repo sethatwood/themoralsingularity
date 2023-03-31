cef8bc177549359403d046b9e9f31232958ee6b5
Seth Atwood <seth.atwood@twinthread.com>
2023-03-31 02:00:20 -0400

    make footer home link rel

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="text-gray-500 text-center mt-16">
         Built by Human+AI collaboration with 💜
         <br />
-        <a href="https://themoralsingularity.org">TheMoralSingularity.org</a>
+        <a href="/">TheMoralSingularity.org</a>
         <span class="hidden md:inline"> • </span><br class="md:hidden" />
         <a href="mailto:hello@themoralsingularity.org">
             hello@themoralsingularity.org
